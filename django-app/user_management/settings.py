62e7c988b270447b9703af86fc9b55e2d665193e
Denis <deniskhuharev@gmail.com>
2025-02-21 16:47:34 +0700

    Reworked disigne and functional

--- django-app/user_management/settings.py
+++ django-app/user_management/settings.py
@@ -166,7 +166,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "users", "static"),
     os.path.join(BASE_DIR, "..", "kanban-board-master", "dist", "assets"),
     REACT_STATIC_DIR,
 ]
